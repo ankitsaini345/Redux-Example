ee828f80d97d21fef324d973b0981c9e49f3ed0c
Ankit Saini <ankit-a.saini@db.com>
2024-08-19 11:58:46 +0530

    added redux toolkit

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,9 @@
-import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import { devToolsEnhancer } from 'redux-devtools-extension';
 import { reducer } from './bugs';
 
-const store = createStore(reducer, devToolsEnhancer({trace: true}));
+// const store = createStore(reducer, devToolsEnhancer({trace: true}));
+
+const store = configureStore({reducer})
 
 export default store;
